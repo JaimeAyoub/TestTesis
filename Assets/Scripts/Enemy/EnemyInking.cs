210f1292f86f7ffd91f82bdac4a1e7e009355fc1
JaimeAyoub <ayoubramosjaime@gmail.com>
2026-01-29 12:15:35 -0600

    Clase 29 Enero

--- Assets/Scripts/Enemy/EnemyInking.cs
+++ Assets/Scripts/Enemy/EnemyInking.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.VFX;
 
+
 public class EnemyInking : MonoBehaviour
 {
     public float amountOfInk = 0.0f;
-    private float _inkForDeath;
-    public VisualEffectAsset deathEffect;
+    private float _inkForDeath = 50.0f;
+    public VisualEffect deathEffect;
     //Aqui tendira que pasarle el shader y qye el amount of ink sea el que diga cuanto "manche" al enemigo.
 
 
     void Start()
     {
+        deathEffect = GetComponent<VisualEffect>();
     }
 
     // Update is called once per frame
@@ -24,6 +26,7 @@ public class EnemyInking : MonoBehaviour
         if (amountOfInk < _inkForDeath)
         {
             amountOfInk += inkToAdd;
+            GetComponent<MeshRenderer>().material.SetFloat("_Radius", amountOfInk/_inkForDeath);
             if (amountOfInk >= _inkForDeath)
             {
                 InkDeath();
@@ -33,6 +36,16 @@ public class EnemyInking : MonoBehaviour
 
     void InkDeath()
     {
-        
+
+     
+       s deathEffect.SendEvent("OnPlay");
+
+        this.transform.DOScale(Vector3.zero, 0.3f);
+
+
+
+
+
+
     }
 }
