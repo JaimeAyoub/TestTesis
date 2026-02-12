5e5ac4054085c9d8a1ddf996aaf0c540cd7a4547
JaimeAyoub <ayoubramosjaime@gmail.com>
2026-02-11 19:12:03 -0600

    Intento de fix

--- Assets/Scripts/HitBoxesGenerator.cs
+++ Assets/Scripts/HitBoxesGenerator.cs
@@ -36,40 +36,81 @@ public class HitBoxesGenerator : MonoBehaviour
         switch (_chooseLimbs)
         {
             case Limbs.leftArm:
-                limbsChosen[0] = leftArm[0];
-                limbsChosen[1] = leftArm[1];
+                if (leftArm.Length > 0)
+                {
+                    limbsChosen[0] = leftArm[0];
+                    limbsChosen[1] = leftArm[1];
+                }
+                else
+                {
+                    Debug.Log("Brazo izquierdo vacio");
+                }
+
                 break;
             case Limbs.rightArm:
-                limbsChosen[0] = rightArm[0];
-                limbsChosen[1] = rightArm[1];
+                if (rightArm.Length > 0)
+                {
+                    limbsChosen[0] = rightArm[0];
+                    limbsChosen[1] = rightArm[1];
+                }
+                else
+                {
+                    Debug.Log("Brazo derecho vacio");
+                }
+
                 break;
             case Limbs.leftLeg:
-                limbsChosen[0] = leftLeg[0];
-                limbsChosen[1] = leftLeg[1];
+                if (leftLeg.Length > 0)
+                {
+                    limbsChosen[0] = leftLeg[0];
+                    limbsChosen[1] = leftLeg[1];
+                }
+                else
+                {
+                    Debug.Log("Pierna izquierda vacio");
+                }
+
                 break;
             case Limbs.rightLeg:
-                limbsChosen[0] = rightLeg[0];
-                limbsChosen[1] = rightLeg[1];
+                if (rightLeg.Length > 0)
+                {
+                    limbsChosen[0] = rightLeg[0];
+                    limbsChosen[1] = rightLeg[1];
+                }
+                else
+                {
+                    Debug.Log("Pierna derecha vacio");
+                }
+
                 break;
             default:
                 break;
         }
 
-        Collider[] hitColliders = Physics.OverlapCapsule(limbsChosen[0].transform.position,
-            limbsChosen[1].transform.position, 0.50f, layermask);
-        if (hitColliders.Length > 0)
+        if (limbsChosen.Length > 0)
         {
-            foreach (Collider collider in hitColliders)
+            Collider[] hitColliders = Physics.OverlapCapsule(limbsChosen[0].transform.position,
+                limbsChosen[1].transform.position, 0.5f, layermask);
+
+
+            if (hitColliders.Length > 0)
             {
-                Debug.Log(collider.name);
+                foreach (Collider collider in hitColliders)
+                {
+                    Debug.Log(collider.name);
+                }
             }
+        }
+        else
+        {
+            Debug.Log("No hay partes seleccionadas");
         }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        
+
         Gizmos.DrawSphere(leftArm[0].transform.position, 0.50f);
         Gizmos.DrawSphere(leftArm[1].transform.position, 0.50f);
 
